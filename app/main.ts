db3a3c04e98e227b3d1d437dc7c463a5e136a750
Zakaria Mouhid <zakariamouhid@gmail.com>
2024-06-10 00:53:53 +0100

    pass 1st stage

--- app/main.ts
+++ app/main.ts
@@ -8,6 +8,6 @@ const server = net.createServer((socket) => {
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-// server.listen(4221, 'localhost', () => {
-//     console.log('Server is running on port 4221');
-// });
+server.listen(4221, 'localhost', () => {
+    console.log('Server is running on port 4221');
+});
