e612424000bd1616b15a01b6bcd397f75ccb129f
Zakaria Mouhid <zakariamouhid@gmail.com>
2024-06-10 02:55:08 +0100

    parse request body

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,7 @@ const statusTextByCode: {
     [code: number]: string;
 } = {
     200: 'OK',
+    201: 'Created',
     404: 'Not Found',
 };
 const commonHeaders = {
@@ -19,7 +20,7 @@ type Handler = (req: Request) => PromiseLike<Response> | Response;
 
 const handler: Handler = async (req: Request) => {
     console.log('req.method', req.method, 'req.url', req.url);
-    if (req.method !== "GET") {
+    if (req.method !== "GET" && req.method !== "POST") {
         return new Response("", { status: 405 });
     }
     const url = new URL(req.url);
@@ -44,7 +45,7 @@ const handler: Handler = async (req: Request) => {
     // echo -n 'Hello, World!' > /tmp/foo
     // curl -i http://localhost:4221/files/foo
     // curl -i http://localhost:4221/files/non_existant_file
-    if (url.pathname.startsWith("/files/")) {
+    if (req.method === "GET" && url.pathname.startsWith("/files/")) {
         const filename = url.pathname.slice("/files/".length);
         if (filename.includes("/") || filename.includes("\\")) {
             return new Response("", { status: 404 });
@@ -60,7 +61,19 @@ const handler: Handler = async (req: Request) => {
                 'content-type': 'application/octet-stream',
             }
         });
-
+    }
+    // curl -v --data "12345" -H "Content-Type: application/octet-stream" http://localhost:4221/files/file_123
+    if (req.method === "POST" && url.pathname.startsWith("/files/")) {
+        const filename = url.pathname.slice("/files/".length);
+        if (filename.includes("/") || filename.includes("\\")) {
+            return new Response("", { status: 404 });
+        }
+        const file = Bun.file(path.resolve(tmpDir, filename));
+        const postBody = await req.arrayBuffer();
+        file.writer().write(postBody);
+        return new Response("", {
+            status: 201,
+        });
     }
     // curl -v http://localhost:4221/404
     return new Response("", { status: 404 });
@@ -68,28 +81,59 @@ const handler: Handler = async (req: Request) => {
 
 const server = net.createServer(async (socket) => {
     console.log('got connection from', socket.remoteAddress, socket.remotePort);
-    const lines = await new Promise<string[]>((resolve) => {
+    const parsedRequest = await new Promise<{
+        head: string;
+        rawHeaders: string[];
+        body: string;
+    }>((resolve) => {
         let data = "";
         let cursor = 0;
-        const lines: string[] = [];
+        let head: string | undefined = undefined;
+        let rawHeaders: string[] = [];
+        let contentLength: number | undefined = undefined;
+        let body: string = "";
+        let endOfHeaders = false;
         socket.on('data', (chunk) => {
             // chunk = "GET /abcdefg HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.84.0\r\nAccept: */*\r\n\r\n"
             data += chunk.toString();
             while (cursor < data.length) {
                 const end = data.indexOf('\r\n', cursor);
                 if (end === -1) {
-                    break;
+                    if (endOfHeaders) {
+                        body = data.slice(cursor);
+                    }
+                } else {
+                    const line = data.slice(cursor, end);
+                    cursor = end + 2;
+                    if (typeof head === 'undefined') {
+                        head = line;
+                    } else if (!endOfHeaders) {
+                        if (line === "") {
+                            endOfHeaders = true;
+                        } else {
+                            rawHeaders.push(line);
+                            const [key, value] = line.split(': ');
+                            if (key.toLowerCase() === "content-length") {
+                                contentLength = parseInt(value, 10);
+                            }
+                        }
+                    } else {
+                        body += line;
+                    }
                 }
-                const line = data.slice(cursor, end);
-                lines.push(line);
-                cursor = end + 2;
-                if (line === '') {
-                    resolve(lines);
+
+                if (typeof head !== "undefined" && typeof contentLength !== "undefined" && body.length >= contentLength) {
+                    resolve({
+                        head,
+                        rawHeaders,
+                        body,
+                    });
+                    return;
                 }
             }
         });
     });
-    const head = lines[0];
+    const { head, rawHeaders, body: rawBody } = parsedRequest;
     const [method, requestPath, httpVersion] = head.split(' ');
     if (['GET', 'POST', 'PUT', 'DELETE'].indexOf(method) === -1) {
         socket.write('HTTP/1.1 405 Method Not Allowed\r\n\r\n');
@@ -101,8 +145,6 @@ const server = net.createServer(async (socket) => {
         socket.end();
         return;
     }
-    const lastHeaderIndex = lines.indexOf('');
-    const rawHeaders = lines.slice(1, lastHeaderIndex);
     // console.log('rawHeaders', rawHeaders);
     // console.log('data', lines);
     const url = new URL(`http://${socket.localAddress}:${socket.localPort}${requestPath}`);
@@ -111,9 +153,12 @@ const server = net.createServer(async (socket) => {
         const [key, value] = rawHeader.split(': ');
         requestHeaders.set(key, value);
     }
+    console.log('rawBody', rawBody);
+    const requestBody = new Uint8Array(Buffer.from(rawBody));
     const req = new Request(url, {
         method,
         headers: requestHeaders,
+        body: requestBody,
     });
     const res = await handler(req);
     const statusCode = res.status;
