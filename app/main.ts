a05fb3be4fce5292000a215e37c6b0788e677feb
Zakaria Mouhid <zakariamouhid@gmail.com>
2024-06-10 01:07:44 +0100

    add http version and status

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,12 @@
 import * as net from 'net';
 
 const server = net.createServer((socket) => {
+    const httpVersion = '1.1';
+    const statusCode = 200;
+    const statusReason = 'OK';
+    const headers = "";
+    const body = "";
+    socket.write(`HTTP/${httpVersion} ${statusCode} ${statusReason}\r\n${headers}\r\n${body}`);
     socket.end();
 });
 
